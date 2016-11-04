7f6a1140d41a86424c4760b37fa3b5a05c13aba7
Pradeep <Pradeep@pradeeploganathan.com>
2016-11-04 12:30:55 +0530

    Second Commit

--- BeerOnTap/BeerOnTap/BeerSearchPage.xaml.cs
+++ BeerOnTap/BeerOnTap/BeerSearchPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BeerOnTap.Models;
+using Newtonsoft.Json;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +14,7 @@ namespace BeerOnTap
     public partial class BeerSearchPage : ContentPage
     {
         Label resultsLabel;
+        List<Beer> Beers;
 
         public BeerSearchPage()
         {
@@ -26,36 +30,51 @@ namespace BeerOnTap
             {
                 Placeholder = "Xamarin.Forms Property",
             };
+
             searchBar.SearchButtonPressed += OnSearchBarButtonPressed;
 
-            resultsLabel = new Label();
-            resultsLabel.Text = "results";
+            ListView BeerList = new ListView();
 
-            // Build the page.
+            BeerList.ItemsSource = Beers;
+
+
+
+
+
+                // Build the page.
             this.Content = new StackLayout
             {
                 Children =
                 {
                     header,
                     searchBar,
-                    new ScrollView
-                    {
-                        Content = resultsLabel,
-                        VerticalOptions = LayoutOptions.FillAndExpand
-                    }
+                    BeerList
+                    
                 }
             };
         }
 
-        void OnSearchBarButtonPressed(object sender, EventArgs args)
+        async void OnSearchBarButtonPressed(object sender, EventArgs args)
         {
+            //JsonSerializer _serializer = JsonSerializer.CreateDefault();
+            //SmartWeb.RestClient rcclient = new SmartWeb.RestClient(new Uri("http://api.brewerydb.com/v2/"), _serializer);
+            //rcclient.AddDefaultQueryString("key", "d9b2bcb52bdeb909e004b988b9e55185");
+            //SmartWeb.RestRequest rrequest = rcclient.Create("beers");
+            //rrequest.AddQueryString("name", "oberon");
+            //RestResponse< ResponseContainer < Beer >> rresponse = await rrequest.ExecuteGet<ResponseContainer<Beer>>();
+            //var item = await rresponse.GetDataObject().ConfigureAwait(false);
+            //resultsLabel.Text = content;
+
+
+            //BreweryDbClient client = new BreweryDbClient("a956af587b434c4c89ef18c7bbd2fac9");
+            //var response = await client.Beers.GetAll();
+            //var beer = response.Data.FirstOrDefault(x => x.Labels != null);
+
             BreweryDbClient client = new BreweryDbClient("a956af587b434c4c89ef18c7bbd2fac9");
+            var response = await client.Beers.Search("duvel");
+            Beers = response.Data;
 
-            Models.ResponseContainer<List<Models.Beer>> results = client.Beers.GetAll().Result;
 
-            resultsLabel.Text = results.Data.ToString();
-            
-            
         }
     }
 }
